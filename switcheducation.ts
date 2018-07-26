8a0045a40c2fadfc7fea6ff9c8dc0f36ce893dd1
Soumura-Kazunori <soumura@switch-science.com>
2018-07-26 16:00:44 +0900

    Add files via upload

--- switcheducation.ts
+++ switcheducation.ts
@@ -370,7 +370,7 @@ namespace SwitchEducation {
     }
 
     //% blockId="switch_education_ledmeter"
-    //% block="LEDを %value |個つける"
+    //% block="LEDを%value|個つける"
     //% value.min=0 value.max=25
     export function ledmeter(value: number): void{
         let x = 0
